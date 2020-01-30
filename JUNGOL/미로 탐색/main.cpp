e1102166fcb549181bf1c085a7621cc53cce3e0b
Rkda8071 <gangminson@naver.com>
2020-01-30 23:12:23 +0900

    Add JUNGOL

--- JUNGOL/미로 탐색/main.cpp
+++ JUNGOL/미로 탐색/main.cpp
@@ -1,13 +1,36 @@
 #include<bits/stdc++.h>
 using namespace std;
 vector<int> a[100100];
+queue<int> q;
 int chk[100100];
+int cnt,n,m;
+void f(int x){
+    if(chk[x]==0){
+        chk[x] = 1;
+        q.push(x);
+        if(++cnt == n) return;
+        sort(a[x].begin(),a[x].end());
+    }
+    int k = a[x].size();
+    for(int i=0;i<k;i++){
+        if(chk[a[x][i]] == 0){
+            f(a[x][i]);
+        }
+    }
+    return;
+}
 int main(){
-    int n,m;
     int x,y;
+    scanf("%d %d",&n,&m);
     for(int i=0;i<m;i++){
         scanf("%d %d",&x,&y);
         a[x].push_back(y);
         a[y].push_back(x);
     }
+    f(1);
+    while(!q.empty()){
+        printf("%d ",q.front());
+        q.pop();
+    }
+    return 0;
 }
