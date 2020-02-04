99cd6bfac381e3c3fb222345452e37f7d16055a9
Rkda8071 <gangminson@naver.com>
2020-02-04 21:37:41 +0900

    Add PS

--- JUNGOL/2462_zltnstj/main.cpp
+++ JUNGOL/2462_zltnstj/main.cpp
@@ -1,39 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
-int n,m;
-vector<int> a[510];
-int s[510][510],l[510][510],c[510];
-void f(int x){
-    s[x][x] = 1; c[x]++;
-    for(int i=a[x].size();i>=0;i--){
-
+int a[510][510],cnt[510];
+int main(){
+    int n,m;
+    int x,y;
+    scanf("%d%d",&n,&m);
+    for(int i=0;i<m;i++){
+        scanf("%d %d",&x,&y);
+        a[x][y] = 1;
+        cnt[x] ++; cnt[y]++;
     }
-    for(int i=1;i<=n;i++){
-        for(int j=a[x].size()-1;j>=0;j--){
-            if(l[a[x][j]][i]){
-
-            }
-        }
-    }
-    for(int i=1;i<=n;i++){
-        if(s[x][i]){
-            for(int j=a[x].size()-1;j>=0;j--){
-                if(s[a[x][j]][i]==0){
-                    c[a[x][j]]++;
-                    s[a[x][j]][i] = 1;
+    for(int k=1;k<=n;k++){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(i==j || i==k || j==k) continue;
+                if(!a[i][j] && a[i][k] && a[k][j]){
+                    a[i][j] = 1;
+                    cnt[i]++; cnt[j]++;
                 }
             }
         }
     }
-}
-int main(){
-    int x,y;
-    scanf("%d %d",&n,&m);
-    for(int i=1;i<=m;i++){
-        scanf("%d %d",&x,&y);
-        a[x].push_back(y);
-    }
-    for(int i=1;i<=n;i++){
-        if(chk[i] == 0) f(i);
-    }
+    for(int i=1;i<=n;i++) if(cnt[i]==n-1) cnt[0]++;
+    printf("%d",cnt[0]);
+    return 0;
 }
