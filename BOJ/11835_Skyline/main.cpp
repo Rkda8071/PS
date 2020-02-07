155fbd9395cec6d3a5f3258317bd730ae345c27e
Rkda8071 <gangminson@naver.com>
2020-02-08 00:34:15 +0900

    Add PS

---
+++ BOJ/11835_Skyline/main.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ll d3[310],d5[310],d7[310],hap;
+int n;
+int main(){
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++) scanf("%lld",&d3[i]);
+    for(int i=1;i<=n;i++){
+        d5[i] = min(d3[i-1],d3[i]); d3[i-1] -= d5[i]; d3[i] -= d5[i];
+        d7[i] = min(d5[i-1],d3[i]); d5[i-1] -= d7[i]; d3[i] -= d7[i];
+    }
+    for(int i=1;i<=n;i++) hap += d3[i]*3 + d5[i]*5 + d7[i]*7;
+    printf("%lld",hap);
+}
