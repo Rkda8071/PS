c8c9ace7c8914aaaa50347e3a861b3be62fd916b
Rkda8071 <gangminson@naver.com>
2021-06-30 22:22:10 +0900

    0630

--- BOJ/21761/main.cpp
+++ BOJ/21761/main.cpp
@@ -2,20 +2,93 @@
 using namespace std;
 typedef long long ll;
 int n,k;
-ll d[4][200100];
+ll d[200100];
 vector<ll> a[4];
-int main(){
+int chk[200100];
+
+void init(){
     scanf("%d %d",&n,&k);
-    for(int i=0;i<4;i++) scanf("%d",&d[i][0]);
+    for(int i=0;i<4;i++){
+        ll x;
+        scanf("%lld",&x);
+        a[i].push_back(x);
+    }
     for(int i=0;i<n;i++){
         char x;
-        int y;
-        scanf(" %c %d",&x,&y);
+        ll y;
+        scanf(" %c %lld",&x,&y);
         a[x-'A'].push_back(y);
     }
     for(int i=0;i<4;i++)
-        sort(a[i].begin(),a[i].end(),greater<int>());
+        sort(a[i].begin()+1,a[i].end(),greater<int>());
+    for(int i=0;i<4;i++)
+        for(int j=1;j<a[i].size();j++)
+            a[i][j] += a[i][j-1];
+}
+void init_d(){
+    int l=a[0].size()-1, r=a[1].size()-1;
+    while(l || r){
+        d[l+r] = a[0][l] * a[1][r];
+        chk[l+r] = r;
+        if(l==0)
+            --r;
+        else if(r==0)
+            --l;
+        else{
+            if(a[0][l-1]*a[1][r]>a[0][l]*a[1][r-1])
+                --l;
+            else
+                --r;
+        }
+    }
+    d[0] = a[0][0] * a[1][0];
+    chk[0] = 0;
+}
+void solve(){
+    ll maxim = 0;
 
+    int l=a[2].size()-1, r=a[3].size()-1,dapr=0,dapl=0;
+    while(l || r){
+        ll ang = a[2][l] * a[3][r]; //l+r
+        if(k>=l+r && d[k-(l+r)]){
+            if(maxim<d[k-(l+r)]*ang){
+                maxim = d[k-(l+r)]*ang;
+                dapr=r; dapl=l;
+            }
+        }
+        if(l==0)
+            --r;
+        else if(r==0)
+            --l;
+        else{
+            if(a[2][l-1]*a[3][r]>a[2][l]*a[3][r-1])
+                --l;
+            else
+                --r;
+        }
+    }
+    ll ang = a[2][l] * a[3][r]; //l+r
+    if(k>=l+r && d[k-(l+r)]){
+        if(maxim<d[k-(l+r)]*ang){
+            maxim = d[k-(l+r)]*ang;
+            dapr=r; dapl=l;
+        }
+    }
+    int ang2 = k-(dapr+dapl);
+
+    for(int i=1;i<=ang2-chk[ang2];i++)
+        printf("A %lld\n",a[0][i]-a[0][i-1]);
+    for(int i=1;i<=chk[ang2];i++)
+        printf("B %lld\n",a[1][i]-a[1][i-1]);
+    for(int i=1;i<=dapl;i++)
+        printf("C %lld\n",a[2][i]-a[2][i-1]);
+    for(int i=1;i<=dapr;i++)
+        printf("D %lld\n",a[3][i]-a[3][i-1]);
+}
+int main(){
+    init();
+    init_d();
+    solve();
 
     return 0;
 }
--- TEST/main.cpp
+++ TEST/main.cpp
@@ -1,64 +1,64 @@
-#include<bits/stdc++.h>
-using namespace std;
-<<<<<<< HEAD
-int n,m;
-int a[30],cnt[30];
-int dap[10];
-void f(int x){
-    if(x==m+1){
-        for(int i=1;i<=m;i++)
-            printf("%c",dap[i] + 'a');
-        puts("");
-        return;
-    }
-    for(int i=0;i<26;i++){
-        if(cnt[i]<a[i]){
-            cnt[i]++;
-            dap[x] = i;
-            f(x+1);
-            cnt[i]--;
-            dap[x] = 0;
+#include<stdio.h>
+int i,j,n;
+int cnt=0,ans=0,t;
+int a[100][100],ch[100][100];
+void f(int x,int y,int tt)
+{
+    ch[x][y]=t;
+    if(a[x][y]>tt)
+    {
+        if((x+1)!=n && ch[x+1][y]!=t)
+        {
+            f(x+1,y,tt);
+        }
+        if((x-1)!=-1 && ch[x-1][y]!=t)
+        {
+            f(x-1,y,tt);
+        }
+        if((y+1)!=n && ch[x][y+1]!=t)
+        {
+            f(x,y+1,tt);
+        }
+        if((y-1)!=-1 && ch[x][y-1]!=t)
+        {
+            f(x,y-1,tt);
         }
     }
+    return;
 }
-int main(){
-    scanf("%d %d",&n,&m);
-    for(int i=0;i<n;i++){
-        char x;
-        scanf(" %c",&x);
-        a[x-'a']++;
+int main()
+{
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
     }
-    //dap[0] = 0;
-    f(1);
-=======
-int n,a[1000001],ar[1000001];
-void f(int i,int sum){
-   if(sum==1){
-      return;
-   }
-   if((a[sum/3]>i||a[sum/3]==0)&&sum%3==0){
-      ar[i]=sum/3;
-      a[sum/3]=i+1;
-      f(i+1,sum/3);
-   }
-   if((a[sum/2]>i||a[sum/2]==0)&&sum%2==0){
-      ar[i]=sum/2;
-      a[sum/2]=i+1;
-      f(i+1,sum/2);
-   }
-   if(a[sum-1]>i||a[sum-1]==0){
-      ar[i]=sum-1;
-      a[sum-1]=i+1;
-      f(i+1,sum-1);
-   }
-}
-int main() {
-   scanf("%d", &n);
-   f(0,n);
-   printf("%d\n%d ",a[1],n);
-   for(int i=1;i>1;i=ar[i]){
-      printf("%d ",ar[i]);
-   }
-    return 0;
->>>>>>> 697ebf68c1277f4c5f32a85f57d6164abbbb3da3
+    for(t=1;t<100;t++)
+    {
+        cnt=0;
+        for(i=0;i<n;i++)
+        {
+            for(j=0;j<n;j++)
+            {
+                if(a[i][j]>t && ch[i][j]!=t)
+                {
+                    f(i,j,t);
+                    cnt++;
+                }
+            }
+        }
+        //printf("qwertygood");
+        if(cnt>ans)
+        {
+            ans=cnt;
+        }
+        if(cnt==0)
+        {
+            break;
+        }
+    }
+    printf("%d",ans);
 }
