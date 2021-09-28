b29bf95cd1dc85d59b483ab8b16eb1b56e79cf8e
Rkda <gangminson@naver.com>
2021-09-28 15:52:16 +0900

    0928

--- BOJ/11375/main.cpp
+++ BOJ/11375/main.cpp
@@ -1,25 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 int n,m,cur;
-vector<int> a[1010];
-int d[1010],cnt[1010],chk[1010];
+vector<int> v[1010];
+int d[1010],chk[1010];
 void dfs(int x){
-    for(auto y: a[x]){
-        if(chk[y] == cur) continue;
-        chk[y] = cur;
-        if(d[y])
-            dfs(d[y]);
-
+    for(auto xx : v[x]){
+        if(chk[xx] == cur) continue;
+        chk[xx] = cur;
+        dfs(d[xx]);
+        d[xx] = x;
+        break;
     }
 }
 void init(){
-    int x,nn;
     scanf("%d %d",&n,&m);
     for(int i=1;i<=n;i++){
-        scanf("%d",&nn);
-        while(nn--){
+        int x,k;
+        scanf("%d",&k);
+        while(k--){
             scanf("%d",&x);
-            a[i].push_back(x);
+            v[i].push_back(x);
         }
     }
 }
@@ -29,9 +29,9 @@ int main(){
         cur = i;
         dfs(i);
     }
-    int dap = 0;
+
+    int sum = 0;
     for(int i=1;i<=n;i++)
-        dap += (d[i]) ? 1 : 0;
-    printf("%d",dap);
-    return 0;
+        sum += (d[i]) ? 1 : 0;
+    printf("%d",sum);
 }
