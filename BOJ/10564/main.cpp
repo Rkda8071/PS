c2b27740fb53d87e54ac21db5b0d071e94967d50
Rkda <gangminson@naver.com>
2021-11-04 20:40:06 +0900

    1104

--- BOJ/10564/main.cpp
+++ BOJ/10564/main.cpp
@@ -1,17 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
-int s[20];
-int d[1000][5010]; // Á¡¼ö, È½¼ö
+int s[11], d[5010][510];
+int n,m;
+int solve(){
+    scanf("%d %d",&n,&m);
+    for(int i=0;i<=n;i++)
+        for(int j=0;j<=500;j++)
+            d[i][j] = -1;
+    d[0][0] = 0;
+    for(int i=1;i<=m;i++) scanf("%d",&s[i]);
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=500;j++){
+            if(d[i][j] == -1) continue;
+            for(int l=1;l<=m;l++){
+                if(i+j+s[l] > n) continue;
+                d[i+j+s[l]][j+s[l]] = 1;
+            }
+        }
+    }
+    for(int i=500;i>=0;i--)
+        if(d[n][i]>0) return i;
+    return -1;
+}
 int main(){
     int t;
     scanf("%d",&t);
-    while(t--){
-        int n,m;
-        scanf("%d %d",&n,&m);
-        for(int i=1;i<=m;i++) scanf("%d",&s[i]);
-        for(int i=0;i<=n;i++){
-
-        }
-    }
+    while(t--)
+        printf("%d\n", solve());
     return 0;
 }
+
