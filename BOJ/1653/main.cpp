2de9aae95f57a01d2900dc57f65bcf51caabf0d4
Rkda <gangminson@naver.com>
2021-12-30 16:06:51 +0900

    1230

--- BOJ/1653/main.cpp
+++ BOJ/1653/main.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 typedef long long ll;
 int n,a[10],k;
-vector<int> v[1<<10][100];
-set<ll> s;
+vector<int> v[1<<10][200];
+vector<ll> s;
 void f(int x,int chk,int hap,int cur){
     if(x == 5){
         v[chk][hap].push_back(cur);
@@ -20,8 +20,7 @@ void g(int x,int chk,int hap,ll cur){
         for(int i=0;i<(1<<9);i++){
             if(chk&i) continue;
             for(int ang : v[i][hap]){
-                s.insert(cur*100000LL + (ll)(ang));
-                if(cur == 0LL) printf("%d\n",hap);
+                s.push_back(cur*100000LL + (ll)(ang));
             }
         }
         return;
@@ -39,10 +38,6 @@ int main(){
     g(0,0,0,0LL);
     scanf("%d",&k);
     k = min(k,(int)s.size()-1);
-    for(auto it = s.begin(); it!=s.end(); it++){
-        if(k-- == 0){
-            printf("%lld",*it);
-            break;
-        }
-    }
+    sort(s.begin(),s.end());
+    printf("%lld",s[k]);
 }
