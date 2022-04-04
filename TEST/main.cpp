a359081c7cb755268570e51b6d00d7d31d19cd1e
Rkda8071 <gangminson@naver.com>
2022-04-04 13:55:36 +0900

    20220404

--- TEST/main.cpp
+++ TEST/main.cpp
@@ -1,1 +1,9 @@
-#i
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cin >> x;
+    // ! ~ & ^ | +
+    //cout << ((~0) ^ x);//((~0) ^ x) + 1;
+    cout << (x & (((~0) ^ x) + 1));
+}
