c723b85fb9b8bf33b6870b23ecf2701249ce5dd9
Rkda <gangminson@naver.com>
2022-05-22 11:18:48 +0900

    20220522

---
+++ BOJ/10xxx/14706.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int l[210], s[210];
+int dp[200010];
+int main(){
+    int n,m;
+    scanf("%d %d",&n,&m);
+    for(int i=1;i<=m;i++)
+        scanf("%d %d",&l[i], &s[i]);
+    
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            
+        }
+    }
+}
--- TEST/main.cpp
+++ TEST/main.cpp
@@ -1,43 +1,27 @@
 #include<iostream>
-
+#include<queue>
 using namespace std;
 
-string in[26];
-int ans[30], chk[30], dp[30], num, n;
-
-int check(int m) {
-   int ans1 = 0;
-   for (int i = 0; i < m; i++) {
-      if (dp[ans[i]]) ans1 = ans1 | dp[ans[i]];
-      else {
-         int ans2 = 0,nn = in[ans[i] - 1].length();
-         for (int j = 0; j < nn; j++)
-            ans2 = ans2 | (1 << (in[ans[i] - 1][j] - 'a'));
-         dp[ans[i]] = ans2;
-         ans1 |= ans2;
-      }
-   }
-   if (ans1 == (1<<26)-1) return 1;
-   else return 0;
-}
-
-void f(int ind, int st) {
-   num += check(ind);
-   for (int i = st; i <= n; i++) {
-      if (chk[i]) continue;
-      chk[i]++;
-      ans[ind] = i;
-      f(ind + 1, i + 1);
-      ans[ind] = 0;
-      chk[i]--;
-   }
-}
+typedef unsigned long long ull;
 
 int main() {
-   cin >> n;
-   for (int i = 0; i < n; i++) cin >> in[i];
-   
-   f(0, 1);
-   
-   cout << num;
+    ios::sync_with_stdio(0); cin.tie(0);
+    ull x, k, ans = 0;
+    cin >> x >> k;
+    int i = 63;
+    queue<int> ind;
+    while (x) {
+        ull t = x % 2ULL;
+        if (!t) ind.push(i);
+        //ans |= ((1ULL - t) << (63 - i--));
+        i--;
+        x /= 2ULL;
+    }
+    while (i) ind.push(i--);
+    while (k) {
+        ans |= ((k % 2ULL) << (63 - ind.front()));
+        ind.pop();
+        k /= 2ULL;
+    }
+    cout << ans;
 }
