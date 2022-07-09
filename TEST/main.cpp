653f709da4842882bf91675379ae53fc3e17045d
Rkda <gangminson@naver.com>
2022-07-09 10:58:49 +0900

    220709

--- TEST/main.cpp
+++ TEST/main.cpp
@@ -1,27 +1,14 @@
-#include<iostream>
-#include<queue>
-using namespace std;
+#include<stdio.h>
 
-typedef unsigned long long ull;
-
-int main() {
-    ios::sync_with_stdio(0); cin.tie(0);
-    ull x, k, ans = 0;
-    cin >> x >> k;
-    int i = 63;
-    queue<int> ind;
-    while (x) {
-        ull t = x % 2ULL;
-        if (!t) ind.push(i);
-        //ans |= ((1ULL - t) << (63 - i--));
-        i--;
-        x /= 2ULL;
-    }
-    while (i) ind.push(i--);
-    while (k) {
-        ans |= ((k % 2ULL) << (63 - ind.front()));
-        ind.pop();
-        k /= 2ULL;
-    }
-    cout << ans;
+void myswap(int &a, int &b){
+    int tmp = a;
+    a = b;
+    b = tmp;
+}
+int main(){
+    int a,b;
+    a = 3; b = 5;
+    printf("%d %d\n",a,b);
+    myswap(a,b);
+    printf("%d %d",a,b);
 }
--- testC/main.c
+++ testC/main.c
@@ -1,32 +1,10 @@
-#include<bits/stdc++.h>
-using namespace std;
-int n;
-char a[21][1001];
-
-struct A{
-    int idx,last;
-    bool operator < (const A &r)const{
-        return strcmp(strcat(a[idx] + last,a[r.idx] + r.last), strcat(a[r.idx] + r.last, a[idx] + last)) > 0;
-    }
-};
-
-priority_queue<A> pq;
-
+#include<stdio.h>
+int a = 5;
+int func(){
+    int a = 4;
+    return a;
+}
 int main(){
-    scanf("%d",&n);
-    for(int i=0;i<n;i++){
-        scanf(" %s",a[i]);
-        pq.push({i,0});
-    }
-    int ang = 0;
-    while(!pq.empty()){
-        int idx, last;
-        idx = pq.top().idx; last = pq.top().last; pq.pop();
-        printf("[%d %d]",idx,last);
-        printf("%c",a[idx][last]);
-        if(a[idx][++last] != '\0')
-            pq.push({idx,last});
-    }
-
-    return 0;
+    int a = 3;
+    printf("%d %d %d",a, func(), a);
 }
