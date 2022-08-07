d5771c9c970c587276e3b64f2aceaf7144ca6b64
Rkda <gangminson@naver.com>
2022-08-07 10:31:59 +0900

    20020807

--- testC/main.c
+++ testC/main.c
@@ -1,10 +1,25 @@
-#include<stdio.h>
-int a = 5;
-int func(){
-    int a = 4;
-    return a;
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include<math.h>
+
+//Direct Sampling
+int try_N = 5000, count = 0;
+
+int main() {
+   srand((unsigned)time(NULL));
+   float x, y;
+   for (int j = 1; j <= 5; j++) {
+      for (int i = 1; i <= try_N; i++) {
+         x = rand() / RAND_MAX;
+         x *= pow(-1, rand());
+         y = rand() / RAND_MAX;
+         y *= pow(-1, rand());
+         
+         if (x * x + y * y <= 1) count++;
+      }
+      printf("%.3f\n", (float)count / try_N);
+   }
 }
-int main(){
-    int a = 3;
-    printf("%d %d %d",a, func(), a);
-}
+
+//x*x+y*y가 1 미만이면 성립
